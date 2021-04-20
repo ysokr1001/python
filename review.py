a61a1cffbf2ea1955d136d980f1284493386c4a5
ysokr1001 <ysokr1001@naver.com>
2021-04-21 00:04:43 +0900

    review2

---
+++ review.py
@@ -0,0 +1,71 @@
+# 음료수 자판기 만들기
+# b = int(input("음료 선택: 1번 사이다, 2번 콜라, 3번 식혜"))
+# if b == 1:
+#     print("콜라를 선택하셨습니다")
+# elif b == 2:
+#     print("사이다를 선택하셨습니다")
+# elif b == 3:
+#     print("식혜를 선택하셨습니다")
+# else:
+#     print("음료를 선택하세요")
+
+# 학생인가 아닌가? 학생이라면?
+# user = int(input("학생인가요? 1예/2아니오"))
+# if user == 1:
+#     print("학생입니다")
+#     stu = int(input("선택하세요: 1번 초등학생, 2번 중학생, 3번 고등학생"))
+#     if stu == 1:
+#         print("초등학생입니다")
+#     elif stu == 2:
+#         print("중학생입니다")
+#     elif stu == 3:
+#         print("고등학생입니다")
+#     else:
+#         print("대학생입니다")
+# else:
+#     print("성인입니다")
+
+# 돈을 넣었을 때, 금액이 차감되는
+# m = int(input("금액을 넣으세요"))
+# user = int(input("1번 사이다, 2번 콜라, 3번 식혜"))
+# if user == 1:
+#     print("잔액은", m-600, "입니다")
+# elif user == 2:
+#     print("잔액은", m-700, "입니다")
+# elif user == 3:
+#     print("잔액은", m-800, "입니다")
+# else:
+#     print("금액을 넣지 않았습니다")
+
+# age = int(input("나이를 입력하세요"))
+# m = 9000
+# if 7 <= age and age <= 12:
+#     print("잔액은", m-450, "입니다")
+# elif 13 <= age and age <= 18:
+#     print("잔액은", m-720, "입니다")
+# elif age >= 19:
+#     print("잔액은", m-1200, "입니다")
+# else:
+#     print("교통요금 대상자가 아닙니다")
+
+# if조건문 예제2
+# user = int(input("값을 입력하시오"))
+# if user + 20 > 250:
+#     print("250 이상의 값입니다")
+# else:
+#     print(user+20,"입니다")
+
+# 정각인 시간을 찾으시오
+# user = input("시간입력")
+# if user[3:] == '00':
+#     print("정각")
+# else:
+#     print("정각아님")
+
+# 좋아하는 과일은? 정답입니다
+# f = ["사과", "포도", "홍시"]
+# user = str(input("좋아하는 과일은?"))
+# if user in f:
+#     print("정답")
+# else:
+#     print("정답아님")
